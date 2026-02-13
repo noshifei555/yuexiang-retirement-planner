eed4f0002922ec27439b883511e5c15a75cf8dd2
noshifei555 <noshifei555@gmail.com>
2026-02-14 00:11:12 +0800

    Create reusable footer component with disclaimer and links placeholder

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div className="disclaimer">
+                <p>This is a placeholder disclaimer for the footer.</p>
+            </div>
+            <div className="links">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
