23c2aa5d89cb6e1364b282f4a0ca6382591500b4
noshifei555 <noshifei555@gmail.com>
2026-02-14 00:10:53 +0800

    Create reusable Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="brand">Brand Name</div>
+            <nav className="navigation">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <div className="options">
+                <select className="language-switch">
+                    <option value="en">EN</option>
+                    <option value="es">ES</option>
+                </select>
+                <button className="mode-switch">Toggle Mode</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
